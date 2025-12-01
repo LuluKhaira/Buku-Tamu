64b0241632d142e43f6da5cb39f27091beb0b2ef
Zahwa Aqila <aqlazhw779@gmail.com>
2025-12-01 08:50:03 +0700

    error edit dan hapus

--- config/riwayat_update.php
+++ config/riwayat_update.php
@@ -1,36 +1,28 @@
 <?php
-header('Content-Type: application/json');
-include 'connect.php';
+include '../config/connect.php';
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    echo json_encode(['success' => false, 'message' => 'Invalid request method']);
-    exit;
-}
+$nama       = $_POST['nama'];
+$no_hp      = $_POST['no_hp'];
+$tanggal    = date("Y-m-d");
+$waktu      = date("H:i:s");
+$instansi   = $_POST['instansi'];
+$tujuan     = $_POST['tujuan'];
+$jenis      = $_POST['jenis'];
+$id         = $_POST['id']; 
 
-$id = isset($_POST['id']) ? (int)$_POST['id'] : 0;
-$nama = isset($_POST['nama']) ? mysqli_real_escape_string($connect, $_POST['nama']) : '';
-$no_hp = isset($_POST['no_hp']) ? mysqli_real_escape_string($connect, $_POST['no_hp']) : '';
-$instansi = isset($_POST['instansi']) ? mysqli_real_escape_string($connect, $_POST['instansi']) : '';
-$tujuan = isset($_POST['tujuan']) ? mysqli_real_escape_string($connect, $_POST['tujuan']) : '';
-$jumlah = isset($_POST['jumlah']) ? (int)$_POST['jumlah'] : 1;
+$query = "UPDATE pengunjung SET 
+            nama='$nama',
+            no_hp='$no_hp',
+            tanggal='$tanggal',
+            waktu='$waktu',
+            instansi='$instansi',
+            tujuan='$tujuan',
+            jenis='$jenis'
+          WHERE id='$id'";
 
-if ($id <= 0) {
-    echo json_encode(['success' => false, 'message' => 'Invalid id']);
-    exit;
-}
+mysqli_query($connect, $query);
 
-$sql = "UPDATE pengunjung SET 
-            nama = '$nama', 
-            no_hp = '$no_hp', 
-            instansi = '$instansi', 
-            tujuan = '$tujuan', 
-            jumlah = $jumlah
-         WHERE id = $id";
-
-if (mysqli_query($connect, $sql)) {
-    echo json_encode(['success' => true, 'message' => 'Data berhasil diperbarui']);
-} else {
-    echo json_encode(['success' => false, 'message' => mysqli_error($connect)]);
-}
-
+// Redirect kembali
+header("Location: ../riwayat_update.php");
+exit();
 ?>

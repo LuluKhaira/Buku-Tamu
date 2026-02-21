ad04104c6561b6776274c2832a7d54715e3a7183
Eza <ringoezra4@gmailcom>
2026-02-21 12:23:48 +0700

    coba

--- tambah_tim.php
+++ tambah_tim.php
@@ -155,8 +155,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="asset/card_tambah_tim.js"></script>
 
-    ckck
-
 </body>
 
 </html>
